1426c130ba2b6b760e298b1c51eee2d1246446ba
Luis Felipe <otaldolu.isborges4522@outlook.com.br>
2024-01-15 17:29:43 -0400

    Exercicio 08

--- Udemy/src/pacote/EstruturaCondicional/Exercícios/Exercicio08.java
+++ Udemy/src/pacote/EstruturaCondicional/Exercícios/Exercicio08.java
@@ -14,30 +14,28 @@ public class Exercicio08 {
 
         double renda = sc.nextDouble();
         
-        if (renda > 0.00 && renda < 2000.01) {
+        if (renda >= 0.00 && renda <= 2000.00) {
             System.out.println("Isento");
         }
         
-        else if (renda > 2000.01 && renda < 3000.01) {
+        else if (renda >= 2000.01 && renda <= 3000.00) {
             
-            renda * 8
+          double imposto = 0.08 * renda;
             
-            System.out.println("calma ai");
+            System.out.printf("R$ %.2f%n", imposto);
         }
         
+        else if (renda >= 3000.01 && renda <= 4500.00 ) {
+            double imposto = 0.18 * renda;
+
+            System.out.printf("R$ %.2f%n", imposto);
+        }
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        else if (renda > 4500.00) {
+            double imposto = 0.28 * renda;
+            
+            System.out.printf("R$ %.2f%n", imposto);
+        }
         
         sc.close();
     }

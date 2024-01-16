0a8d596cf2427bd03223a8bc8929f982ae7a559f
Luis Felipe <otaldolu.isborges4522@outlook.com.br>
2024-01-16 10:57:13 -0400

    Finalizado exercícios seção 05, Estrutura Condicional

--- Udemy/src/pacote/EstruturaCondicional/Exercícios/Exercicio08.java
+++ Udemy/src/pacote/EstruturaCondicional/Exercícios/Exercicio08.java
@@ -13,30 +13,31 @@ public class Exercicio08 {
         System.out.println("Informe a renda: ");
 
         double renda = sc.nextDouble();
-        
+        double imposto;
+
         if (renda >= 0.00 && renda <= 2000.00) {
             System.out.println("Isento");
         }
         
         else if (renda >= 2000.01 && renda <= 3000.00) {
             
-          double imposto = 0.08 * renda;
-            
+            imposto = 0.08 * renda;
             System.out.printf("R$ %.2f%n", imposto);
         }
         
         else if (renda >= 3000.01 && renda <= 4500.00 ) {
-            double imposto = 0.18 * renda;
-
+           
+            imposto = 0.08 * 1000.00 + 0.18 * 2.00;
             System.out.printf("R$ %.2f%n", imposto);
         }
         
         else if (renda > 4500.00) {
-            double imposto = 0.28 * renda;
             
+            imposto = 0.08 * 1000.00 + 0.18 * 1500.00 + 0.28 * 20.00;
             System.out.printf("R$ %.2f%n", imposto);
         }
         
         sc.close();
     }
 }
+ 

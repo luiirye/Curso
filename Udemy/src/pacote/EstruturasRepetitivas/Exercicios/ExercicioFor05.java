d827b4ebc7149ab32d9ce3d488eddccfa1c96406
Luis Felipe <otaldolu.isborges4522@outlook.com.br>
2024-02-13 16:58:30 -0400

    gap

--- Udemy/src/pacote/EstruturasRepetitivas/Exercicios/ExercicioFor05.java
+++ Udemy/src/pacote/EstruturasRepetitivas/Exercicios/ExercicioFor05.java
@@ -8,20 +8,16 @@ public class ExercicioFor05 {
         Scanner sc = new Scanner (System.in);
 
 
+        int N = sc.nextInt();
+        int fatorial = 1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for (int i = 1; i <= N ; i++) {
+         
+            fatorial = fatorial * i;
+           
+            System.out.println(fatorial);
+            
+        }
         sc.close();
     }
 }
